08f5c4b93736fe7ee4f11ff6daf3fc50a8fcca33
0Cymantek0 <cymantekfnosz@gmail.com>
2025-10-19 20:06:37 +0530

    ok

--- ai-extension/src/components/ui/tooltip.tsx
+++
@@ -1,2 +0,0 @@
-// Deprecated re-export: use animate-ui Tooltip everywhere
-export { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider } from "@/components/animate-ui/components/animate/tooltip";
